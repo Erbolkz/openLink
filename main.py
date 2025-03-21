298f8245e9729f206cc5003cfe40684dc125795d
Yerbol Zhunussov <zhunusovem@mail.ru>
2025-03-21 22:19:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
-response = requests.get("https://p1.nisproject.kz")
-print(response.text)
+#response = requests.get("https://p1.nisproject.kz")
+#print(response.text)
 
 response = requests.get("https://aiteacher.kz/unchecked")
 print(response.text)
